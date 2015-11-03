fd4a60b18e54760970ca931ad023952996313194
Vlad Bokov <razum2um@mail.ru>
2015-11-03 19:48:46 +0600

    setup figwheel

--- project.clj
+++ project.clj
@@ -5,7 +5,18 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
+                 [ring "1.4.0"]
+                 [compojure "1.4.0"]
+                 [http-kit "2.1.19"]
+                 [org.omcljs/om "1.0.0-alpha4"]
+                 [aprint "0.1.3"]
+                 [reloaded.repl "0.2.1"]
                  [figwheel-sidecar "0.5.0-SNAPSHOT"]]
-  :main ^:skip-aot jsconf.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all
+                       :main ^:skip-aot jsconf.core}}
+  :checkout-deps-shares ^:replace [:source-paths :resource-paths :compile-path]
+
+  :plugins [[lein-figwheel "0.5.0-SNAPSHOT"]]
+  :source-paths ["src/clj" "src/cljs" "src/cljc"]
+  )
